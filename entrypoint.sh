788bcda73eee430ba4ee70e4c8ff6304ed1d2c0b
franspaco <franspaco@gmail.com>
2018-05-01 16:50:51 -0500

    deployment ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,29 @@
+#!/bin/bash
 
-if [ ! -d restaurant-backend]; then
+cd
+
+server_dir="restaurant-backend"
+
+if [ ! -d "$server_dir" ]; then
     git clone https://github.com/franspaco/restaurant-backend.git
+    cd restaurant-backend
+else
+    cd restaurant-backend
+    git pull origin master
 fi
 
-cd restaurant-backend
-flask run --port 80 --host $HOST
+
+#required because python
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+# flask
+export FLASK_APP=project
+
+# defaults
+export MONGO_HOST=${MONGO_HOST:-"localhost"}
+export MONGO_PORT=${MONGO_PORT:-"27017"}
+export HOST=${HOST:-"0.0.0.0"}
+export PORT=${PORT:-"80"}
+
+flask run --port $PORT --host $HOST
