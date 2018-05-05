f3c499643680936bf304dad4cd8ea1f32dd05215
franspaco <franspaco@gmail.com>
2018-05-05 18:15:59 -0500

    + Add customer to tab

--- project/models/tab.py
+++ project/models/tab.py
@@ -16,6 +16,27 @@ class Tab:
 
     def toDict(self):
         return self.__dict__
+
+    def addCustomer(self, username):
+        usr = User.user_from_username(username)
+        if not usr:
+            return False
+        
+        # Check for uniqueness
+        if usr.id in [val['id'] for val in self.customers]:
+            return False
+
+        new_data = {
+            "id": usr.id,
+            "name": usr.name,
+            "username": usr.username
+        }
+        self.customers.append(new_data)
+        result = get_db().tabs.update_one({'_id':ObjectId(self.id)},{'$push':{'customers':new_data}})
+        if result.modified_count == 1:
+            return True
+        else:
+            return False
     
     @staticmethod
     def create(waiter_usr, table_no, creation_time, customers=None):
@@ -23,6 +44,7 @@ class Tab:
         customer_list = list()
 
         if isinstance(customers, list):
+            customers = list(set(customers))
             for val in customers:
                 usr = User.user_from_username(val)
                 if not usr:
