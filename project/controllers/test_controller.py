8c391de3f6f34b2e7f086e6b05523c0864304a2f
franspaco <franspaco@gmail.com>
2018-05-06 02:26:30 -0500

    irrelevant changes

--- project/controllers/test_controller.py
+++ project/controllers/test_controller.py
@@ -1,11 +1,15 @@
 
 
-from flask import Blueprint, request, abort, current_app
+from flask import Blueprint, request, abort, current_app, make_response
 from project.db import get_db
 from bson import json_util
 
 bp = Blueprint('test', __name__)
 
 @bp.route('/', methods=['Get', 'POST'])
-def user_login():
+def test():
     return "<h1> Hello world! </h1>"
+
+@bp.route('/dope')
+def wat():
+    abort(make_response("Lol", 420))
