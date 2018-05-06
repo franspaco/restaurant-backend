27675ee30994fb55e8783f4229b38af70332a8c7
franspaco <franspaco@gmail.com>
2018-05-06 01:01:51 -0500

    + tables/available

--- project/controllers/tables_controller.py
+++ project/controllers/tables_controller.py
@@ -1,6 +1,6 @@
 
 from flask import Blueprint, request, abort, make_response, jsonify
-from project.db import save_kv, load_kv
+from project.db import save_kv, load_kv, get_db
 from project.helpers import req_helper
 
 bp = Blueprint('tables', __name__)
@@ -21,3 +21,18 @@ def tables_set(number):
         req_helper.throw_not_allowed()
     save_kv('tables', number)
     return jsonify(message='Ok!')
+
+@bp.route('/available', methods=['POST'])
+def tables_get_available():
+    user = req_helper.force_session_get_user()
+    if not user.is_admin():
+        req_helper.throw_not_allowed()
+
+    tables = load_kv('tables')
+
+    used_tables = get_db().tabs.find().distinct("table")
+
+    available = [ i for i in range(1, tables+1) if i not in used_tables]
+
+    return jsonify(available)
+
