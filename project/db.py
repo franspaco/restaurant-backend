10e7300503ff7a2252004e634c95c1a9539d6431
franspaco <franspaco@gmail.com>
2018-05-06 00:34:39 -0500

    + kv store

--- project/db.py
+++ project/db.py
@@ -7,3 +7,16 @@ def get_db():
     if 'db' not in g:
         g.db = MongoClient(os.environ['MONGO_HOST'], int(os.environ['MONGO_PORT'])).restaurants
     return g.db
+
+def save_kv(key, value):
+    kvstore = get_db().kvstore
+    kvstore.replace_one({'key':key}, {"key":key, "value": value}, upsert=True)
+
+def load_kv(key):
+    kvstore = get_db().kvstore
+    res = kvstore.find_one({'key':key})
+
+    if res is None:
+        return None
+    else:
+        return res['value']
